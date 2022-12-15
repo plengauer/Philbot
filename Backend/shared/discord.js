bd4337c80aed8c9ef35884685d326518c3d9c481
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2022-12-15 13:34:09 +0100

    ripped out the retry and opentelemetry logic from the communication layer to ease refactoring

--- Backend/shared/discord.js
+++ Backend/shared/discord.js
@@ -1,7 +1,3 @@
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-const opentelemetry = require('@opentelemetry/api');
-const tracer = opentelemetry.trace.getTracer('discord');
-const retry = require('./retry.js').retry;
 const permissions = require('./permissions.js');
 
 function parse_mention(mention) {
@@ -10,26 +6,11 @@ function parse_mention(mention) {
   return mention.substring(2, mention.length - 1);
 }
 
-async function retry_with_rate_limit(func) {
-  return retry(func, e => e.message.includes('rate limited') || e.message.includes('503: Service Unavailable'));
-}
-
-async function guild_retrieve_0(guild_id) {
-  return retry_with_rate_limit(() => lib.discord.guilds['@0.2.4'].retrieve({ guild_id: guild_id }));
-}
-
 async function guild_retrieve(guild_id) {
-  let span = tracer.startSpan('discord.guild.retrieve');
-  span.setAttribute('discord.guild.id', guild_id);
-  return opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), span), () => guild_retrieve_0(guild_id))
-    .finally(() => span.end());
+  return lib.discord.guilds['@0.2.4'].retrieve({ guild_id: guild_id });
 }
 
-async function guilds_list_paged(limit, first, last) {
-  return retry_with_rate_limit(() => lib.discord.guilds['@0.2.2'].list({ limit: limit, before: first, after: last }));
-}
-
-async function guilds_list_0() {
+async function guilds_list() {
   let limit = 100;
   let guilds = await guilds_list_paged(limit, undefined, undefined);
   if (guilds.length == limit) {
@@ -53,17 +34,11 @@ async function guilds_list_0() {
   return guilds;
 }
 
-async function guilds_list() {
-  let span = tracer.startSpan('discord.guilds.list');
-  return opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), span), guilds_list_0)
-    .finally(() => span.end());
+async function guilds_list_paged(limit, first, last) {
+  return lib.discord.guilds['@0.2.2'].list({ limit: limit, before: first, after: last });
 }
 
-async function guild_members_list_paged(guild_id, limit, last) {
-  return retry_with_rate_limit(() => lib.discord.guilds['@0.2.2'].members.list({ guild_id: guild_id, limit: limit, after: last }));
-}
-
-async function guild_members_list_0(guild_id, role) {
+async function guild_members_list(guild_id, role = undefined) {
   let limit = 100;
   let members = await guild_members_list_paged(guild_id, limit, undefined);
   if (members.length == limit) {
@@ -80,37 +55,19 @@ async function guild_members_list_0(guild_id, role) {
   return members.filter(member => !role || member.roles.includes(role));
 }
 
-async function guild_members_list(guild_id, role = undefined) {
-  let span = tracer.startSpan('discord.guild.members.list');
-  span.setAttribute('discord.guild.id', guild_id);
-  return opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), span), () => guild_members_list_0(guild_id, role))
-    .finally(() => span.end());
-}
-
-async function guild_member_retrieve_0(guild_id, user_id) {
-  return retry_with_rate_limit(() => lib.discord.guilds['@0.2.4'].members.retrieve({ guild_id: guild_id, user_id: user_id }));
+async function guild_members_list_paged(guild_id, limit, last) {
+  return lib.discord.guilds['@0.2.2'].members.list({ guild_id: guild_id, limit: limit, after: last });
 }
 
 async function guild_member_retrieve(guild_id, user_id) {
-  let span = tracer.startSpan('discord.guild.member.retrieve');
-  span.setAttribute('discord.guild.id', guild_id);
-  span.setAttribute('discord.user.id', user_id);
-  return opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), span), () => guild_member_retrieve_0(guild_id, user_id))
-    .finally(() => span.end());
-}
-
-async function guild_roles_list_0(guild_id) {
-  return retry_with_rate_limit(() => lib.discord.guilds['@0.2.4'].roles.list({ guild_id: guild_id }));
+  return lib.discord.guilds['@0.2.4'].members.retrieve({ guild_id: guild_id, user_id: user_id });
 }
 
 async function guild_roles_list(guild_id) {
-  let span = tracer.startSpan('discord.guild.roles.list');
-  span.setAttribute('discord.guild.id', guild_id);
-  return opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), span), () => guild_roles_list_0(guild_id))
-    .finally(() => span.end());
+  return lib.discord.guilds['@0.2.4'].roles.list({ guild_id: guild_id });
 }
 
-async function users_list_0(get_role_async) {
+async function users_list(get_role_async) {
   return guilds_list()
     .then(guilds => guilds.map(guild => (get_role_async ? get_role_async(guild.id) : Promise.resolve(null)).then(role => guild_members_list(guild.id, role))))
     .then(results => Promise.all(results))
@@ -127,33 +84,37 @@ async function users_list_0(get_role_async) {
     });
 }
 
-async function users_list(get_role_async = undefined) {
-  let span = tracer.startSpan('discord.users.list');
-  return opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), span), () => users_list_0(get_role_async))
-    .finally(() => span.end());
-}
-
-async function voice_track_play_0(guild_id, voice_channel_id, download_info) {
-  return retry(
-    () => lib.discord.voice['@0.0.1'].tracks.play({
-        guild_id: guild_id,
-        channel_id: voice_channel_id,
-        download_info: download_info 
-    }), e => e.stack.includes('timed out') || e.stack.includes('try again later')
-  );
-}
-
 async function voice_track_play(guild_id, voice_channel_id, download_info) {
-  let span = tracer.startSpan('discord.voice.track.play');
-  span.setAttribute('discord.guild.id', guild_id);
-  span.setAttribute('discord.channel.id', voice_channel_id);
-  return opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), span), () => 
-    voice_track_play_0(guild_id, voice_channel_id, download_info)
-  ).finally(() => span.end());
+  return lib.discord.voice['@0.0.1'].tracks.play({
+      guild_id: guild_id,
+      channel_id: voice_channel_id,
+      download_info: download_info
+    });
 }
 
 async function scheduledevents_list(guild_id) {
-  return retry_with_rate_limit(() => lib.discord.scheduledevents['@0.0.1'].list({ guild_id: guild_id }));
+  return lib.discord.scheduledevents['@0.0.1'].list({ guild_id: guild_id });
+}
+
+async function respond(channel_id, event_id, content, tts = false) {
+  let limit = 2000;
+  while (content.length > limit) {
+    let index = getSplitIndex(content, limit);
+    await respond_paged(channel_id, event_id, content.substring(0, index), tts);
+    content = content.substring(index + (index < content.length && content[index] === '\n' ? 1 : 0), content.length);
+  }
+  return respond_paged(channel_id, event_id, content, tts);
+}
+
+async function respond_paged(channel_id, event_id, content, tts) {
+  return lib.discord.channels['@0.2.0'].messages.create({
+      channel_id: channel_id,
+      content: content,
+      tts: tts,
+      message_reference: {
+        message_id: event_id
+      }
+    }).then(result => remove_embeds(result.channel_id, result.id, content).then(() => result));
 }
 
 function getSplitIndex(string, limit) {
@@ -164,6 +125,16 @@ function getSplitIndex(string, limit) {
   if (index <= 0) index = string.substring(0, limit - 1).lastIndexOf(' ') + 1;
   if (index <= 0) index = limit;
   return index;
+}
+
+async function remove_embeds(channel_id, message_id, content) {
+  if (!channel_id) return Promise.resolve();
+  if (content && count(content, 'http://') + count(content, 'https://') < 2 && count(content, 'https://discord.com/events/') == 0) return Promise.resolve();
+  return lib.discord.channels['@0.3.2'].messages.update({
+      channel_id: channel_id,
+      message_id: message_id,
+      flags: 1 << 2 // SUPPRESS_EMBEDS
+    }).catch(ex => { /* mimimi - just swallow */ });
 }
 
 function count(haystack, needle) {
@@ -181,63 +152,11 @@ function count(haystack, needle) {
   return count;
 }
 
-async function remove_embeds(channel_id, message_id, content) {
-  if (!channel_id) return Promise.resolve();
-  if (content && count(content, 'http://') + count(content, 'https://') < 2 && count(content, 'https://discord.com/events/') == 0) return Promise.resolve();
-  return lib.discord.channels['@0.3.2'].messages.update({
-      channel_id: channel_id,
-      message_id: message_id,
-      flags: 1 << 2 // SUPPRESS_EMBEDS
-    }).catch(ex => { /* mimimi - just swallow */ });
-}
-
-async function respond_paged(channel_id, event_id, content, tts) {
-  return retry_with_rate_limit(() => lib.discord.channels['@0.2.0'].messages.create({
-      channel_id: channel_id,
-      content: content,
-      tts: tts,
-      message_reference: {
-        message_id: event_id
-      }
-    })
-    .then(result => remove_embeds(result.channel_id, result.id, content).then(() => result))
-  );
-}
-
-async function respond_0(channel_id, event_id, content, tts) {
-  let limit = 2000;
-  while (content.length > limit) {
-    let index = getSplitIndex(content, limit);
-    await respond_paged(channel_id, event_id, content.substring(0, index), tts);
-    content = content.substring(index + (index < content.length && content[index] === '\n' ? 1 : 0), content.length);
-  }
-  return respond_paged(channel_id, event_id, content, tts);
-}
-
-async function respond(channel_id, event_id, content, tts = false) {
-  let span = tracer.startSpan('discord.respond');
-  span.setAttribute('discord.channel.id', channel_id);
-  span.setAttribute('discord.event.id', channel_id);
-  span.setAttribute('discord.tts', tts);
-  return opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), span), () => 
-    respond_0(channel_id, event_id, content, tts)
-  ).finally(() => span.end());
-}
-
 async function post(channel_id, content, tts = false) {
   return respond(channel_id, undefined, content, tts);
 }
 
-async function dms_paged(user_id, content) {
-  return retry_with_rate_limit(() => lib.discord.users['@0.2.0'].dms.create({
-      recipient_id: user_id,
-      content: content
-    })
-    .then(result => remove_embeds(result.channel_id, result.id, content).then(() => result))
-  );
-}
-
-async function dms_0(user_id, content) {
+async function dms(user_id, content) {
   let limit = 2000;
   while (content.length > limit) {
     let index = getSplitIndex(content, limit);
@@ -247,12 +166,12 @@ async function dms_0(user_id, content) {
   return dms_paged(user_id, content);
 }
 
-async function dms(user_id, content) {
-  let span = tracer.startSpan('discord.direct_message');
-  span.setAttribute('discord.user.id', user_id);
-  return opentelemetry.context.with(opentelemetry.trace.setSpan(opentelemetry.context.active(), span), () => 
-    dms_0(user_id, content)
-  ).finally(() => span.end());
+async function dms_paged(user_id, content) {
+  return lib.discord.users['@0.2.0'].dms.create({
+      recipient_id: user_id,
+      content: content
+    })
+    .then(result => remove_embeds(result.channel_id, result.id, content).then(() => result));
 }
 
 async function try_dms(user_id, content) {
@@ -264,18 +183,6 @@ async function try_dms(user_id, content) {
     });
 }
 
-function guild_member_has_permission_0(guild, roles, member, permission) {
-  return guild.owner_id == member.user.id || roles
-    .filter(role => member.roles.includes(role.id))
-    .map(role => permissions.decompile(role.permissions))
-    .some(permission_names => permission_names.includes(permission) || permission_names.includes('ADMINISTRATOR'))
-}
-
-async function guild_member_has_permission(guild_id, user_id, permission) {
-  return Promise.all([ guild_retrieve(guild_id), guild_roles_list(guild_id), guild_member_retrieve(guild_id, user_id) ])
-    .then(data => guild_member_has_permission_0(data[0], data[1], data[2], permission));
-}
-
 async function guild_members_list_with_permission(guild_id, permission) {
   return guild_members_list_with_any_permission(guild_id, [ permission ]);
 }
@@ -285,4 +192,16 @@ async function guild_members_list_with_any_permission(guild_id, permissions) {
     .then(data => data[1].filter(member => permissions.some(permission => guild_member_has_permission_0(data[0], data[2], member, permission))));
 }
 
-module.exports = { parse_mention, retry_with_rate_limit, guild_retrieve, guilds_list, guild_members_list, guild_member_retrieve, guild_roles_list, users_list, voice_track_play, scheduledevents_list, post, respond, dms, try_dms, guild_member_has_permission, guild_members_list_with_permission, guild_members_list_with_any_permission }
+async function guild_member_has_permission(guild_id, user_id, permission) {
+  return Promise.all([ guild_retrieve(guild_id), guild_roles_list(guild_id), guild_member_retrieve(guild_id, user_id) ])
+    .then(data => guild_member_has_permission_0(data[0], data[1], data[2], permission));
+}
+
+function guild_member_has_permission_0(guild, roles, member, permission) {
+  return guild.owner_id == member.user.id || roles
+    .filter(role => member.roles.includes(role.id))
+    .map(role => permissions.decompile(role.permissions))
+    .some(permission_names => permission_names.includes(permission) || permission_names.includes('ADMINISTRATOR'))
+}
+
+module.exports = { parse_mention, guild_retrieve, guilds_list, guild_members_list, guild_member_retrieve, guild_roles_list, users_list, voice_track_play, scheduledevents_list, post, respond, dms, try_dms, guild_member_has_permission, guild_members_list_with_permission, guild_members_list_with_any_permission }
