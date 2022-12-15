a8847e16c6b3cce92a3d8b7dcbaa333fd00bf2a7
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2022-12-15 16:22:37 +0100

    refactored existing functionality from communication layer to use direct HTTP communication

--- Backend/shared/discord.js
+++ Backend/shared/discord.js
@@ -6,36 +6,16 @@ function parse_mention(mention) {
   return mention.substring(2, mention.length - 1);
 }
 
+async function me() {
+  return HTTP(`/users/@me`, 'GET');
+}
+
 async function guild_retrieve(guild_id) {
-  return lib.discord.guilds['@0.2.4'].retrieve({ guild_id: guild_id });
+  return HTTP(`/guilds/${guild_id}`, 'GET');
 }
 
 async function guilds_list() {
-  let limit = 100;
-  let guilds = await guilds_list_paged(limit, undefined, undefined);
-  if (guilds.length == limit) {
-    let first = guilds[0].id;
-    let last = guilds[guilds.length - 1].id;
-    let again = true;
-    while (again) {
-      let more = await guilds_list_paged(limit, first, undefined);
-      guilds = more.concat(guilds);
-      first = guilds[0].id;
-      again = more.length == limit;
-    }
-    again = true;
-    while (again) {
-      let more = await guilds_list_paged(limit, undefined, last);
-      guilds = guilds.concat(more);
-      last = guilds[guilds.length - 1].id;
-      again = more.length == limit;
-    }
-  }
-  return guilds;
-}
-
-async function guilds_list_paged(limit, first, last) {
-  return lib.discord.guilds['@0.2.2'].list({ limit: limit, before: first, after: last });
+  return HTTP(`/users/@me/guilds`, 'GET');
 }
 
 async function guild_members_list(guild_id, role = undefined) {
@@ -56,15 +36,15 @@ async function guild_members_list(guild_id, role = undefined) {
 }
 
 async function guild_members_list_paged(guild_id, limit, last) {
-  return lib.discord.guilds['@0.2.2'].members.list({ guild_id: guild_id, limit: limit, after: last });
+  return HTTP(`/guilds/${guild_id}/members?limit=${limit}&after=${last}`, 'GET');
 }
 
 async function guild_member_retrieve(guild_id, user_id) {
-  return lib.discord.guilds['@0.2.4'].members.retrieve({ guild_id: guild_id, user_id: user_id });
+  return HTTP(`/guilds/${guild_id}/members/${user_id}`, 'GET');
 }
 
 async function guild_roles_list(guild_id) {
-  return lib.discord.guilds['@0.2.4'].roles.list({ guild_id: guild_id });
+  return HTTP(`/guilds/${guild_id}/roles`, 'GET');
 }
 
 async function users_list(get_role_async) {
@@ -85,15 +65,11 @@ async function users_list(get_role_async) {
 }
 
 async function voice_track_play(guild_id, voice_channel_id, download_info) {
-  return lib.discord.voice['@0.0.1'].tracks.play({
-      guild_id: guild_id,
-      channel_id: voice_channel_id,
-      download_info: download_info
-    });
+  throw new Error('NOT IMPLEMENTED!);
 }
 
 async function scheduledevents_list(guild_id) {
-  return lib.discord.scheduledevents['@0.0.1'].list({ guild_id: guild_id });
+  return HTTP(`/guilds/${guild_id}/scheduled-events?with_user_count=false`, 'GET');
 }
 
 async function respond(channel_id, event_id, content, tts = false) {
@@ -107,14 +83,12 @@ async function respond(channel_id, event_id, content, tts = false) {
 }
 
 async function respond_paged(channel_id, event_id, content, tts) {
-  return lib.discord.channels['@0.2.0'].messages.create({
-      channel_id: channel_id,
+  return HTTP(`/channels/${channel_id}/messages`, 'POST', {
       content: content,
       tts: tts,
-      message_reference: {
-        message_id: event_id
-      }
-    }).then(result => remove_embeds(result.channel_id, result.id, content).then(() => result));
+      message_reference: { message_id: event_id, fail_if_not_exists: false }
+      flags: 1 << 2 // SUPPRESS_EMBEDS
+    });
 }
 
 function getSplitIndex(string, limit) {
@@ -125,31 +99,6 @@ function getSplitIndex(string, limit) {
   if (index <= 0) index = string.substring(0, limit - 1).lastIndexOf(' ') + 1;
   if (index <= 0) index = limit;
   return index;
-}
-
-async function remove_embeds(channel_id, message_id, content) {
-  if (!channel_id) return Promise.resolve();
-  if (content && count(content, 'http://') + count(content, 'https://') < 2 && count(content, 'https://discord.com/events/') == 0) return Promise.resolve();
-  return lib.discord.channels['@0.3.2'].messages.update({
-      channel_id: channel_id,
-      message_id: message_id,
-      flags: 1 << 2 // SUPPRESS_EMBEDS
-    }).catch(ex => { /* mimimi - just swallow */ });
-}
-
-function count(haystack, needle) {
-  let count = 0;
-  let index = 0;
-  while (index < haystack.length) {
-    let f = haystack.indexOf(needle, index);
-    if (f >= 0) {
-      count++;
-      index = f + needle.length;
-    } else {
-      index = haystack.length;
-    }
-  }
-  return count;
 }
 
 async function post(channel_id, content, tts = false) {
@@ -167,11 +116,8 @@ async function dms(user_id, content) {
 }
 
 async function dms_paged(user_id, content) {
-  return lib.discord.users['@0.2.0'].dms.create({
-      recipient_id: user_id,
-      content: content
-    })
-    .then(result => remove_embeds(result.channel_id, result.id, content).then(() => result));
+  return HTTP(`/users/@me/channels`, 'POST', { recipient_id: user_id })
+    .then(dm_channel => post(dm_channel.id, content);
 }
 
 async function try_dms(user_id, content) {
@@ -202,6 +148,10 @@ function guild_member_has_permission_0(guild, roles, member, permission) {
     .filter(role => member.roles.includes(role.id))
     .map(role => permissions.decompile(role.permissions))
     .some(permission_names => permission_names.includes(permission) || permission_names.includes('ADMINISTRATOR'))
+}
+
+async function HTTP(endpoint, method, payload = undefined) {
+  return curl.do(method, 'discord.com', '/api/v10' + endpoint, { 'accept': 'application/json', 'content-type': 'application/json', 'authorization': 'Bot ' + process.env.DISCORD_TOKEN }, payload); //TODO would this just like parsing also take care of serializing?
 }
 
 module.exports = { parse_mention, guild_retrieve, guilds_list, guild_members_list, guild_member_retrieve, guild_roles_list, users_list, voice_track_play, scheduledevents_list, post, respond, dms, try_dms, guild_member_has_permission, guild_members_list_with_permission, guild_members_list_with_any_permission }
