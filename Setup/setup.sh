29b0cf2fae5d156824942ed5fa0172246d88006f
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2022-12-20 12:48:00 +0100

    modified setup

--- Setup/setup.sh
+++ Setup/setup.sh
@@ -1,3 +1,3 @@
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - &&
 sudo apt-get -y install unzip nodejs ruby
-# TODO npms and gems
