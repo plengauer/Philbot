d8031a033309d08066878a969150339ff78291b6
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2022-12-22 15:09:43 +0100

    better handling of env vars with spaces

--- Setup/run_nodejs.sh
+++ Setup/run_nodejs.sh
@@ -3,7 +3,7 @@ directory=$2
 cd $directory &&
 npm install $package &&
 npm update &&
-export $(cat environment.properties | xargs) &&
+export $(cat environment.properties | tr ' ' '_' | tr '\n' ' ') &&
 export SERVICE_VERSION=$(cat node_modules/$package/package.json | jq -r .version) &&
 cd node_modules/$package &&
 npm start
--- Setup/run_ruby.sh
+++ Setup/run_ruby.sh
@@ -4,6 +4,6 @@ cd $directory &&
 rm -rf gems &&
 mkdir -p gems &&
 gem install --install-dir=gems $gem
-export $(cat environment.properties | xargs) &&
+export $(cat environment.properties | tr ' ' '_' | tr '\n' ' ') &&
 export SERVICE_VERSION= &&
 ruby gems/gems/$gem-*/*.rb
