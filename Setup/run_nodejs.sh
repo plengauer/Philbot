694e1acb69f8b6c1c2efaabe479fb3a1bb4af38e
philipp.lengauer <p.lengauer@gmail.com>
2022-12-23 14:52:39 +0100

    handle properties with blanks correctly

--- Setup/run_nodejs.sh
+++ Setup/run_nodejs.sh
@@ -3,7 +3,7 @@ directory=$2
 cd $directory &&
 npm install $package &&
 npm update &&
-export $(cat environment.properties | tr ' ' '_' | tr '\n' ' ') &&
+eval $(cat environment.properties | awk '{print "export \"" $0 "\""}') &&
 export SERVICE_VERSION=$(cat node_modules/$package/package.json | jq -r .version) &&
 cd node_modules/$package &&
 npm start
--- Setup/run_ruby.sh
+++ Setup/run_ruby.sh
@@ -3,7 +3,7 @@ directory=$2
 cd $directory &&
 rm -rf gems &&
 mkdir -p gems &&
-gem install --install-dir=gems $gem
-export $(cat environment.properties | tr ' ' '_' | tr '\n' ' ') &&
+gem install --install-dir=gems $gem &&
+eval $(cat environment.properties | awk '{print "export \"" $0 "\""}') &&
 export SERVICE_VERSION= &&
 ruby gems/gems/$gem-*/*.rb
