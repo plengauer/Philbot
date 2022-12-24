982f39df38dc77b66f922e5003063d0a7d4db6f1
philipp.lengauer <p.lengauer@gmail.com>
2022-12-24 14:57:52 +0100

    fixed string comparison

--- Setup/run.sh
+++ Setup/run.sh
@@ -4,7 +4,7 @@ module=$3
 cd $directory &&
 eval $(cat environment.properties | awk '{print "export \"" $0 "\""}') ||
 exit $?
-if [ $technology -eq "node.js" ]
+if [ $technology = "node.js" ]
 then
     package=$module
     npm install $package &&
@@ -12,13 +12,14 @@ then
     export SERVICE_VERSION=$(cat node_modules/$package/package.json | jq -r .version) &&
     cd node_modules/$package &&
     npm start
-elif [ $technology -eq "ruby" ]
+elif [ $technology = "ruby" ]
+then
     gem=$module
     rm -rf gems &&
     mkdir -p gems &&
     gem install --install-dir=gems $gem &&
     export SERVICE_VERSION= &&
     ruby gems/gems/$gem-*/*.rb
-then
+else
     exit 1
 fi
