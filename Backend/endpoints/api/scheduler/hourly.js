0fda2013dae9ea571e214ab42c84319dec15962a
philipp.lengauer <p.lengauer@gmail.com>
2022-12-25 21:39:06 +0100

    fixed require

--- Backend/endpoints/api/scheduler/hourly.js
+++ Backend/endpoints/api/scheduler/hourly.js
@@ -3,7 +3,7 @@ const delayed_memory = require('../../../shared/delayed_memory.js');
 const discord = require('../../../shared/discord.js');
 const datefinder = require('../../../shared/datefinder.js');
 const features = require('../../../shared/features.js');
-const subscriptions = require('../../shared/subscriptions.js');
+const subscriptions = require('../../../shared/subscriptions.js');
 
 const mute_ttl = 60 * 60 * 24 * 7 * 4;
 const scheduling_distance = 1000 * 60 * 60 * 24 * 4;
