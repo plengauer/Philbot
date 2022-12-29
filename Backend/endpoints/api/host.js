a0be98caa5fa7e77f0b366a372588dcc65f2d35f
philipp.lengauer <p.lengauer@gmail.com>
2022-12-29 09:37:06 +0100

    cache host info

--- Backend/endpoints/api/host.js
+++ Backend/endpoints/api/host.js
@@ -2,7 +2,7 @@ const curl = require('../../shared/curl.js');
 
 async function handle() {
   try {
-    let metadata = JSON.parse(await curl.request({ secure: false, hostname: '169.254.169.254', path: '/latest/dynamic/instance-identity/document' })); // they always respond with plain/text
+    let metadata = JSON.parse(await curl.request({ secure: false, hostname: '169.254.169.254', path: '/latest/dynamic/instance-identity/document', cache: 60 * 60 * 24 })); // they always respond with plain/text
     let link = `https://${metadata.region}.console.aws.amazon.com/ec2-instance-connect/ssh?connType=standard&instanceId=${metadata.instanceId}&osUser=ubuntu&region=${metadata.region}&sshPort=22`;
     return {
       status: 302,
