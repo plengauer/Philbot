2bbf8f5d7f68f3a0e501c62227b189334a7eff59
philipp.lengauer <p.lengauer@gmail.com>
2023-01-01 09:57:21 +0100

    added dynamic checks whether public url is available

--- Backend/shared/identity.js
+++ Backend/shared/identity.js
@@ -3,6 +3,7 @@ const curl = require('./curl.js');
 
 async function getPublicURL() {
   return new Promise((resolve, reject) => process.env.PUBLIC_URL ? resolve(process.env.PUBLIC_URL) : reject(new Error()))
+    .then(url => curl.request({ secure: false, method: 'GET', hostname: url.substring(url.indexOf('://') + 3), path: '/' }).then(result => new Promise((resolve, reject) => result.includes('<title>Philbot</title>') ? resolve(url) : reject(new Error()))))
     .catch(error => curl.request({ secure: false, method: 'GET', hostname: '169.254.169.254', path: '/latest/meta-data/public-hostname', headers: { accept: 'text/plain' }, cache: 60 * 60 * 24 }).then(result => 'http://' + result.trim()))
     .catch(error => curl.request({ secure: false, method: 'GET', hostname: 'icanhazip.com', path: '/', headers: { accept: 'text/plain' }, cache: 60 * 60 * 24 }).then(result => 'http://' + result.trim()))
     .catch(error => 'http://127.0.0.1')
