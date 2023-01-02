b1f8c7761de1664d95f22e3f41f171169e484cf9
philipp.lengauer <p.lengauer@gmail.com>
2023-01-02 10:02:07 +0100

    fixed ssh and server endpoint

--- Backend/endpoints/api/server.js
+++ Backend/endpoints/api/server.js
@@ -2,7 +2,7 @@ const curl = require('../../shared/curl.js');
 
 async function handle() {
   try {
-    let response = await curl.request({ secure: false, method: 'GET', hostname: '169.254.169.254', path: '/latest/dynamic/instance-identity/document', timeout: 100, fail_on_timeout: false, cache: 60 * 60 * 24 });
+    let response = await curl.request_simple({ secure: false, method: 'GET', hostname: '169.254.169.254', path: '/latest/dynamic/instance-identity/document', timeout: 100, fail_on_timeout: false, cache: 60 * 60 * 24 });
     if (response.status != 200) throw new Error('Not AWS');
     let metadata = JSON.parse(response.body); // they always respond with plain/text
     let link = `https://${metadata.region}.console.aws.amazon.com/ec2/home?region=${metadata.region}#InstanceDetails:instanceId=${metadata.instanceId}`;
--- Backend/endpoints/api/ssh.js
+++ Backend/endpoints/api/ssh.js
@@ -3,7 +3,7 @@ const identity = require('../../shared/identity.js');
 
 async function handle() {
   try {
-    let response = await curl.request({ secure: false, method: 'GET', hostname: '169.254.169.254', path: '/latest/dynamic/instance-identity/document', timeout: 100, fail_on_timeout: false, cache: 60 * 60 * 24 });
+    let response = await curl.request_simple({ secure: false, method: 'GET', hostname: '169.254.169.254', path: '/latest/dynamic/instance-identity/document', timeout: 100, fail_on_timeout: false, cache: 60 * 60 * 24 });
     if (response.status != 200) throw new Error('Not AWS');
     let metadata = JSON.parse(response.body); // they always respond with plain/text
     let link = `https://${metadata.region}.console.aws.amazon.com/ec2-instance-connect/ssh?connType=standard&instanceId=${metadata.instanceId}&osUser=ubuntu&region=${metadata.region}&sshPort=22`;
