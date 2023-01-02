d9f110776679244fd443906f3b1edd5dd38f9c4e
philipp.lengauer <p.lengauer@gmail.com>
2023-01-02 14:29:44 +0100

    introduced caching

--- Backend/shared/identity.js
+++ Backend/shared/identity.js
@@ -4,13 +4,13 @@ const curl = require('./curl.js');
 
 async function getPublicURL() {
   return new Promise((resolve, reject) => process.env.PUBLIC_URL ? resolve(process.env.PUBLIC_URL) : reject(new Error()))
-    .then(url => curl.request({ secure: false, method: 'GET', hostname: url.substring(url.indexOf('://') + 3), path: '/' }).then(result => new Promise((resolve, reject) => result.includes('<title>Philbot</title>') ? resolve(url) : reject(new Error()))))
+    .then(url => curl.request({ secure: false, method: 'GET', hostname: url.substring(url.indexOf('://') + 3), path: '/', cache: 60 }).then(result => new Promise((resolve, reject) => result.includes('<title>Philbot</title>') ? resolve(url) : reject(new Error()))))
     .catch(error =>
-      curl.request_simple({ secure: false, method: 'GET', hostname: '169.254.169.254', path: '/latest/meta-data/public-hostname', headers: { accept: 'text/plain' }, timeout: 100, fail_on_timeout: false, cache: 60 * 60 * 24 })
+      curl.request_simple({ secure: false, method: 'GET', hostname: '169.254.169.254', path: '/latest/meta-data/public-hostname', headers: { accept: 'text/plain' }, timeout: 100, fail_on_timeout: false, cache: 60 })
         .then(response => response.status == 200 ? Promise.resolve(response.body) : Promise.reject(new Error('Not AWS')))
         .then(result => 'http://' + result.trim())
     )
-    .catch(error => curl.request({ secure: false, method: 'GET', hostname: 'icanhazip.com', path: '/', headers: { accept: 'text/plain' }, cache: 60 * 60 * 24 }).then(result => 'http://' + result.trim()))
+    .catch(error => curl.request({ secure: false, method: 'GET', hostname: 'icanhazip.com', path: '/', headers: { accept: 'text/plain' }, cache: 60 }).then(result => 'http://' + result.trim()))
     .catch(error => 'http://127.0.0.1')
 }
 
