cc4ceb9c8ef9bf3b1a0ca573165808abe9a0d0cf
philipp.lengauer <p.lengauer@gmail.com>
2023-01-07 14:54:01 +0100

    re-disabled voice functionality

--- Backend/endpoints/api/discord/message_create.js
+++ Backend/endpoints/api/discord/message_create.js
@@ -389,7 +389,7 @@ async function handleBuiltInCommand(guild_id, channel_id, event_id, user_id, use
       );
   
   } else if (message.startsWith('play ')) {
-    // return discord.respond(channel_id, event_id, "Voice functionality has been temporarily disabled. I'm working on restoring it as soon as possible!");
+    if (user_id != process.env.OWNER_DISCORD_USER_ID) return discord.respond(channel_id, event_id, "Voice functionality has been temporarily disabled. I'm working on restoring it as soon as possible!");
     if (!guild_id) return reactNotOk(channel_id, event_id);
     if (!await features.isActive(guild_id, 'player')) return respondNeedsFeatureActive(channel_id, event_id, 'player', 'play music');
     message = message.split(' ').slice(1).join(' ');
@@ -410,12 +410,14 @@ async function handleBuiltInCommand(guild_id, channel_id, event_id, user_id, use
       .catch(error => discord.respond(channel_id, event_id, error.message));
       
   } else if (message === "stop") {
+    if (user_id != process.env.OWNER_DISCORD_USER_ID) return discord.respond(channel_id, event_id, "Voice functionality has been temporarily disabled. I'm working on restoring it as soon as possible!");
     guild_id = guild_id ?? await resolveGuildID(user_id);
     if (!guild_id) return reactNotOk(channel_id, event_id);
     if (!await features.isActive(guild_id, 'player')) return respondNeedsFeatureActive(channel_id, event_id, 'player', 'play music');
     return player.stop(guild_id).then(command => reactOK(channel_id, event_id).then(() => command));
     
   } else if (message === "pause") {
+    if (user_id != process.env.OWNER_DISCORD_USER_ID) return discord.respond(channel_id, event_id, "Voice functionality has been temporarily disabled. I'm working on restoring it as soon as possible!");
     return discord.respond(channel_id, event_id, "Voice functionality has been temporarily disabled. I'm working on restoring it as soon as possible!");
     guild_id = guild_id ?? await resolveGuildID(user_id);
     if (!guild_id) return reactNotOk(channel_id, event_id);
@@ -423,6 +425,7 @@ async function handleBuiltInCommand(guild_id, channel_id, event_id, user_id, use
     return player.pause(guild_id).then(() => reactOK(channel_id, event_id));
     
   } else if (message === "resume") {
+    if (user_id != process.env.OWNER_DISCORD_USER_ID) return discord.respond(channel_id, event_id, "Voice functionality has been temporarily disabled. I'm working on restoring it as soon as possible!");
     return discord.respond(channel_id, event_id, "Voice functionality has been temporarily disabled. I'm working on restoring it as soon as possible!");
     guild_id = guild_id ?? await resolveGuildID(user_id);
     if (!guild_id) return reactNotOk(channel_id, event_id);
