a90265c1c7d94b45d96c5ae18e3ff97ad3c6bac8
philipp.lengauer <p.lengauer@gmail.com>
2023-01-09 23:48:42 +0100

    fixed parsing for connect event

--- Backend/shared/role_management.js
+++ Backend/shared/role_management.js
@@ -113,7 +113,7 @@ async function parse_trigger_role(parser, guild_id) {
 
 async function parse_trigger_connect(parser, guild_id) {
     if (parser_next(parser) != 'connect') throw new Error('Expected "connect"');
-    let channel_name = parser_next(channel_id);
+    let channel_name = parser_next(parser);
     let channel_id = null;
     if (channel_name == 'any') {
         channel_id = null;
