181ab16430eea11f6d253dbf0a59e96bc116b986
philipp.lengauer <p.lengauer@gmail.com>
2023-01-14 17:52:37 +0100

    refactored heartbeating

--- Voice/philbot-voice/voice.py
+++ Voice/philbot-voice/voice.py
@@ -121,51 +121,46 @@ class Context:
 
     ws = None
     socket = None
+    heartbeat_interval = None
     ssrc = None
     ip = None
     port = None
     mode = None
     secret_key = None
 
+    heartbeater = None
+    listener = None
+    streamer = None
+
     def __init__(self, guild_id):
         self.guild_id = guild_id
 
-    def __run_server(self):
-        my_socket = self.socket
-        if my_socket == None:
-            return
+    def __listen(self):
+        print('VOICE CONNECTION listening')
         while True:
             with self.lock:
-                if (my_socket != self.socket):
+                if not self.listener:
                     break
             try:
-                data, address = my_socket.recvfrom(UDP_MAX_PAYLOAD)
+                data, address = self.socket.recvfrom(UDP_MAX_PAYLOAD)
                 # print('VOICE CONNECTION received voice data package from ' + address[0] + ':' + str(address[1]) + ': ' + str(len(data)) + 'b')
-            except:
-                break
-        print('VOICE CONNECTION terminated')
+            except: # TODO limit to socket closed exceptions only
+                pass
+        print('VOICE CONNECTION listener terminated')
 
-    def __stream(self, url, ssrc, secret_key, ip, port):
+    def __stream(self):
         # https://discord.com/developers/docs/topics/voice-connections#encrypting-and-sending-voice
-        my_socket = self.socket
-        if my_socket == None:
-            return
-        if not url.startswith('file://'):
+        if not self.url.startswith('file://'):
             raise RuntimeError
         print('VOICE CONNECTION streaming')
         filename = self.url[len('file://'):]
-        if not url.endswith('.wav') and not url.endswith('.wave'):
+        if not self.url.endswith('.wav') and not self.url.endswith('.wave'):
             raise RuntimeError
-        secret_box = nacl.secret.SecretBox(bytes(secret_key))
+        secret_box = nacl.secret.SecretBox(bytes(self.secret_key))
         sequence = 0
         # https://github.com/Rapptz/discord.py/blob/master/discord/voice_client.py
         package_duration = 20
         file = wave.open(filename, "rb")
-        # encoder = pyogg.opus.OpusEncoder()
-        # encoder.set_application("audio")
-        # encoder.set_sampling_frequency(file.getframerate())
-        # encoder.set_channels(file.getnchannels())
-        # https://www.opus-codec.org/docs/html_api/group__opusencoder.html
         if (file.getframerate() != 48000):
             file.close()
             subprocess.run(['mv', filename, filename + '.backup']).check_returncode()
@@ -178,6 +173,8 @@ class Context:
             raise RuntimeError('unexpected channel count: ' + str(file.getnchannels()))
         if file.getsampwidth() != 2:
             raise RuntimeError('unexpected sample width: ' + str(file.getsampwidth()))
+        if self.mode != "xsalsa20_poly1305":
+            raise RuntimeError('unexpected mode: ' + self.mode)
         print('VOICE CONNECTION streaming ' + filename + ' (' + str(file.getnframes() / file.getframerate() / 60) + 'mins)')
         error = ctypes.c_int(0)
         encoder = pyogg.opus.opus_encoder_create(
@@ -187,11 +184,12 @@ class Context:
             ctypes.byref(error)
         )
         if error.value != 0:
-            raise RuntimeError(str(error))
+            raise RuntimeError(str(error.value))
         buffer = b"\x00" * 1024 * 1024
         desired_frame_duration = package_duration / 1000
         desired_frame_size = int(desired_frame_duration * file.getframerate())
         timestamp = time_millis()
+        last_heartbeat = 0
         while True:
             pcm = file.readframes(desired_frame_size)
             if len(pcm) == 0:
@@ -199,7 +197,6 @@ class Context:
             effective_frame_size = len(pcm) // file.getsampwidth() // file.getnchannels()
             if effective_frame_size < desired_frame_size:
                 pcm += b"\x00" * (desired_frame_size - effective_frame_size) * file.getsampwidth() * file.getnchannels()
-            # opus = encoder.encode(pcm)
             encoded_bytes = pyogg.opus.opus_encode(encoder,
                 ctypes.cast(pcm, pyogg.opus.opus_int16_p),
                 ctypes.c_int(effective_frame_size),
@@ -207,11 +204,23 @@ class Context:
                 pyogg.opus.opus_int32(len(buffer))
             )
             opus = bytes(buffer[:encoded_bytes])
-            package = create_voice_package(sequence, sequence * desired_frame_size, ssrc, secret_box, opus)
+            package = create_voice_package(sequence, sequence * desired_frame_size, self.ssrc, secret_box, opus)
             with self.lock:
-                if (my_socket != self.socket):
+                if not self.streamer:
                     break
-                my_socket.sendto(package, (ip, port))
+            try:
+                self.socket.sendto(package, (self.ip, self.port))
+            except: # TODO limit to socket close exceptions only
+                pass
+            if last_heartbeat + self.heartbeat_interval // 2 <= time_millis():
+                try:
+                    last_heartbeat = time_millis()
+                    self.ws.send(json.dumps({ "op": 3, "d": sequence }))
+                    # self.ws.send(json.dumps({ "op": 3, "d": sequence * package_duration }))
+                    # self.ws.send(json.dumps({ "op": 3, "d": int(sequence * desired_frame_duration) }))
+                    # self.ws.send(json.dumps({ "op": 3, "d": sequence * desired_frame_size }))
+                except: # TODO limit to socket close exceptions
+                    pass
             new_timestamp = time_millis()
             sleep_time = package_duration - (new_timestamp - timestamp)
             if sleep_time < 0:
@@ -227,21 +236,22 @@ class Context:
         file.close()
         print('VOICE CONNECTION stream completed')
 
+    def __heartbeat(self):
+        start = time_millis()
+        while True:
+            time.sleep(self.heartbeat_interval / 1000)
+            with self.lock:
+                if not self.heartbeater:
+                    break
+            print('VOICE GATEWAY sending heartbeat')
+            try: 
+                self.ws.send(json.dumps({ "op": 3, "d": time_millis() - start }))
+            except: # TODO limit to socket close exceptions only
+                pass
+
     def __ws_on_open(self, ws):
         with self.lock:
             print('VOICE GATEWAY connection established')
-
-    def __heartbeat(self, interval):
-        start = time_millis()
-        my_ws = self.ws
-        while True:
-            time.sleep(interval / 1000.0)
-            with self.lock:
-                if (self.ws != None and my_ws == self.ws):
-                    print('VOICE GATEWAY sending heartbeat')
-                    self.ws.send(json.dumps({ "op": 4, "d": time_millis() - start }))
-                else:
-                    return
 
     def __ws_on_message(self, ws, message):
         with self.lock:
@@ -251,8 +261,9 @@ class Context:
                 case 8:
                     print('VOICE GATEWAY received hello')
                     payload = json.loads(message)
-                    heartbeat_interval = payload['d']['heartbeat_interval']
-                    threading.Thread(target=self.__heartbeat, kwargs={ 'interval': heartbeat_interval }).start()
+                    self.heartbeat_interval = payload['d']['heartbeat_interval']
+                    #self.heartbeater = threading.Thread(target=self.__heartbeat)
+                    #self.heartbeater.start()
                     print('VOICE GATEWAY sending identify')
                     ws.send(json.dumps({
                         "op": 0,
@@ -271,8 +282,6 @@ class Context:
                     self.ip = payload['d']['ip']
                     self.port = payload['d']['port']
                     modes = payload['d']['modes']
-                    if ("xsalsa20_poly1305" not in modes):
-                        raise RuntimeError('Mode not supported')
                     my_ip = requests.get('https://ipv4.icanhazip.com/').text.strip()
                     my_port = None
                     print('VOICE CONNECTION opening UDP socket')
@@ -284,8 +293,9 @@ class Context:
                             break
                         except:
                             my_port = None
-                    threading.Thread(target=self.__run_server).start()
                     print('VOICE CONNECTION server ready at ' + my_ip + ':' + str(my_port))
+                    self.listener = threading.Thread(target=self.__listen)
+                    self.listener.start()
                     print('VOICE GATEWAY sending select protocol')
                     ws.send(json.dumps({
                         "op": 1,
@@ -301,8 +311,6 @@ class Context:
                 case 4:
                     print('VOICE GATEWAY received session description')
                     self.mode = payload['d']['mode']
-                    if self.mode != "xsalsa20_poly1305":
-                        raise RuntimeError('unexpected mode: ' + self.mode)
                     self.secret_key = payload['d']['secret_key']
                     print('VOICE GATEWAY sending speaking')
                     ws.send(json.dumps({
@@ -313,8 +321,8 @@ class Context:
                             "ssrc": self.ssrc
                         }
                     }))
-                    time.sleep(1) # give the discord server time to catch up
-                    threading.Thread(target=self.__stream, kwargs = { 'url': self.url, 'ssrc': self.ssrc, 'secret_key': self.secret_key, 'ip': self.ip, 'port': self.port }).start()
+                    self.streamer = threading.Thread(target=self.__stream)
+                    self.streamer.start()
                 case 5:
                     print('VOICE GATEWAY received speaking')
                     # nothing else to do ...
@@ -331,33 +339,50 @@ class Context:
             self.ws.close()
 
     def __ws_on_close(self, ws, close_code, close_message):
+        print('VOICE GATEWAY connection closing (' + str(close_code) + ': ' + close_message + ')')
+        heartbeater = None
+        listener = None
+        streamer = None
         with self.lock:
-            print('VOICE GATEWAY connection closed (' + str(close_code) + ': ' + close_message + ')')
-            self.ws = None
-            if (self.socket != None):
+            heartbeater = self.heartbeater
+            listener = self.listener
+            streamer = self.streamer
+            self.listener = None
+            self.streamer = None
+            self.heartbeater = None
+            if self.socket:
                 self.socket.close()
+        if heartbeater:
+            heartbeater.join()
+        if listener:
+            listener.join()
+        if streamer:
+            streamer.join()
+        with self.lock:
             self.socket = None
             self.ssrc = None
             self.ip = None
             self.port = None
             self.ssrc = None
             self.secret_key = None
+            self.ws = None
+        print('VOICE GATEWAY connection closed')
+        with self.lock:
             self.__try_start() # if we closed intentionally, channel id will be null
     
     def __try_start(self):
-        if (self.ws != None or self.channel_id == None or self.session_id == None or self.endpoint == None or self.token == None or self.url == None):
+        if self.ws or not self.channel_id or not self.session_id or not self.endpoint or not self.token or not self.url:
             return
         self.ws = websocket.WebSocketApp(self.endpoint + '?v=4', on_open=self.__ws_on_open, on_message=self.__ws_on_message, on_error=self.__ws_on_error, on_close=self.__ws_on_close)
         threading.Thread(target=self.ws.run_forever).start()
     
     def __stop(self):
-        if (self.ws == None):
+        if not self.ws:
             return
         self.ws.close()
 
     def on_server_update(self, endpoint, token):
         with self.lock:
-            # TODO this will reset the content - is that fine?
             self.endpoint = endpoint
             self.token = token
             self.__stop()
@@ -368,16 +393,17 @@ class Context:
             self.channel_id = channel_id
             self.user_id = user_id
             self.session_id = session_id
-            if (self.channel_id == None):
+            if not self.channel_id:
+                self.endpoint = None
+                self.token = None
                 self.__stop()
             else:
                 self.__try_start()
 
     def on_content_update(self, url):
         with self.lock:
-            self.url = url
-            # TODO is this really correct? it should be but it could maybe be made more efficient but just resetting the streamed file
             self.__stop()
+            self.url = url
             self.__try_start()
 
 contexts_lock = threading.Lock()
