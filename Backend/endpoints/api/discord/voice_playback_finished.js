bee374b5e502918366bcd597f8f5870abf9f3ea8
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2023-01-16 10:00:02 +0100

    really stop when playback is finished

--- Backend/endpoints/api/discord/voice_playback_finished.js
+++ Backend/endpoints/api/discord/voice_playback_finished.js
@@ -1,8 +1,12 @@
 const player = require('../../../shared/player.js');
+const discord = require('../../../shared/discord.js');
+
+(await memory.get(`voice_channel:user:${user_id}`, null))?.channel_id
 
 async function handle(payload) {
-  return player.playNext(payload.guild_id, payload.user_id)
-    .then(() => undefined);
+  return discord.me()
+    .then(me => memory.get(`voice_channel:user:${me.id}`))
+    .then(voice_status => voice_status?.channel_id ? player.playNext(payload.guild_id, payload.user_id) : undefined)
 }
 
 module.exports = { handle }
