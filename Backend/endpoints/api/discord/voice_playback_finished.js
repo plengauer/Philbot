7128b54c66303f9eb9b026f7516e5d1246e38135
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2023-01-16 10:18:56 +0100

    fixed playback finished

--- Backend/endpoints/api/discord/voice_playback_finished.js
+++ Backend/endpoints/api/discord/voice_playback_finished.js
@@ -1,12 +1,10 @@
 const player = require('../../../shared/player.js');
 const discord = require('../../../shared/discord.js');
 
-(await memory.get(`voice_channel:user:${user_id}`, null))?.channel_id
-
 async function handle(payload) {
   return discord.me()
     .then(me => memory.get(`voice_channel:user:${me.id}`))
-    .then(voice_status => voice_status?.channel_id ? player.playNext(payload.guild_id, payload.user_id) : undefined)
+    .then(voice_status => voice_status?.channel_id ? player.playNext(payload.guild_id, payload.user_id) : player.stop())
 }
 
 module.exports = { handle }
