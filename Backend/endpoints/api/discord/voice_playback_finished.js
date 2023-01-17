c4f964c8405c86e92d2be5cb099880aa90c2a233
philipp.lengauer <p.lengauer@gmail.com>
2023-01-17 23:05:43 +0100

    added missing parameter to stop

--- Backend/endpoints/api/discord/voice_playback_finished.js
+++ Backend/endpoints/api/discord/voice_playback_finished.js
@@ -5,7 +5,7 @@ const player = require('../../../shared/player.js');
 async function handle(payload) {
   return discord.me()
     .then(me => memory.get(`voice_channel:user:${me.id}`))
-    .then(voice_status => voice_status?.channel_id ? player.playNext(payload.guild_id, payload.user_id) : player.stop())
+    .then(voice_status => voice_status?.channel_id ? player.playNext(payload.guild_id, payload.user_id) : player.stop(payload.guild_id))
 }
 
 module.exports = { handle }
