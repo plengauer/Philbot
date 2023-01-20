b6768e7a65ca2f0613c153580020341e2c298d51
philipp.lengauer <p.lengauer@gmail.com>
2023-01-20 15:16:44 +0100

    fix access to event name

--- DiscordGateway2HTTP/discordgateway2http.js
+++ DiscordGateway2HTTP/discordgateway2http.js
@@ -52,7 +52,7 @@ function handleMessage(state, message) {
         shard: SHARD_INDEX, 
         code: event.op,
         name: event.t?.toLowerCase().replace(/_/g, ' ') ?? "",
-        guild_id: event.d.guild_id ?? (event.startsWith('GUILD_') ? event.d.id : ""),
+        guild_id: event.d.guild_id ?? (event.t.startsWith('GUILD_') ? event.d.id : ""),
         user_id: event.d.user_id ?? event.d.user?.id ?? event.d.member?.user?.id ?? event.d.author?.id ?? (event.startsWith('USER_') ? event.d.id : ""),
         activities: payload.activities?.map(activity => activity.name).join(',')
     });
