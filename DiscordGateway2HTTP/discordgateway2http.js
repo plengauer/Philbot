0c231982d9b5708f4cb40e7182436976d2ece5db
philipp.lengauer <p.lengauer@gmail.com>
2023-01-22 20:25:09 +0100

    properly close servers so that the new ones can bind and we connect to the right socket

--- DiscordGateway2HTTP/discordgateway2http.js
+++ DiscordGateway2HTTP/discordgateway2http.js
@@ -17,7 +17,7 @@ const STATE_FILE = (process.env.STATE_STORAGE_DIRECTORY ?? './') + '.state.' + S
 connect(restoreState());
 
 async function connect(prev_state = {}) {
-    prev_state.server?.closeAllConnections();
+    await new Promise(resolve => prev_state.server?.close(resolve));
     let state = {
         session_id: prev_state.session_id,
         sequence: prev_state.sequence,
