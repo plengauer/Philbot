92d223865e5d57c011a9617692bd41f10a3e7795
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2023-01-24 09:02:22 +0100

    exec to save processes and memory on ultra-thin server

--- Setup/run.sh
+++ Setup/run.sh
@@ -10,7 +10,7 @@ then
     ([ -e node_modules/ ] || npm install $package) &&
     npm update &&
     export SERVICE_VERSION=$(cat node_modules/$package/package.json | jq -r .version) &&
-    npm --prefix node_modules/$package start
+    exec npm --prefix node_modules/$package start
 elif [ $technology = "ruby" ]
 then
     gem=$module
@@ -19,14 +19,14 @@ then
     gem install --install-dir=gems $gem &&
     pushd gems/gems/$gem-*/ && bundle install && popd &&
     export SERVICE_VERSION= &&
-    ruby gems/gems/$gem-*/lib/*.rb
+    exec ruby gems/gems/$gem-*/lib/*.rb
 elif [ $technology = "python" ]
 then
     package=$module
     pip3 install $package --upgrade &&
     export PYTHONPATH=$(find ~/.local/lib/python*/site-packages/$package -prune | tr '\n' ':') &&
     export SERVICE_VERSION=$(pip show $package | grep Version | cut -d':' -f2 | sed 's/ //g') &&
-    ~/.local/bin/opentelemetry-instrument python3 -u -m $package
+    exec ~/.local/bin/opentelemetry-instrument python3 -u -m $package
 else
     exit 1
 fi
