ce597461f3daa345ba4cdc1444e6cbfd2d1f1042
philipp.lengauer <p.lengauer@gmail.com>
2023-04-04 22:13:33 +0200

    added better handling of gpt pricing

--- Backend/endpoints/api/discord/message_create.js
+++ Backend/endpoints/api/discord/message_create.js
@@ -171,13 +171,13 @@ async function handleMessage(guild_id, channel_id, event_id, user_id, user_name,
     promises.push(promise);
   }
   
-  if (Math.random() < 0.01 && !mentioned && guild_id && message.length > 10 && message.length < 150) {
+  if (!mentioned && guild_id && message.length > 10 && message.length < 150 && await chatgpt.canGetResponse()) {
     let promise = chatgpt.getResponse(null, null, `Is "${message}" exactly one proper sentence and, assuming people enjoy innuendo jokes, is it funny to respond with "That's what she said!" to it? Respond with only yes or no.`)
       .then(response => response ?? '')
       .then(response => response.toLowerCase())
       .then(response => response.endsWith('.') ? response.substring(0, response.length - 1) : response)
       .then(response => {
-        console.log(`DEBUG INNUENDO v2 "${message}" => ${response}`)
+        console.log(`DEBUG INNUENDO v3 "${message}" => ${response}`)
         return response;
       })
     	.then(response => (response == 'yes') ? discord.respond(channel_id, event_id, 'That\'s what she said!') : undefined);
--- Backend/shared/chatgpt.js
+++ Backend/shared/chatgpt.js
@@ -2,26 +2,75 @@ const process = require('process');
 const memory = require('./memory.js');
 const curl = require('./curl.js');
 
+const cost_limit = parseFloat(process.env.OPENAI_API_COST_LIMIT ?? '1.00');
+
+async function canGetResponse(threshold = 0.8) {
+  return (await getCurrentCost()).value / cost_limit * threshold < computeBillingSlotProgress();
+}
+
+function computeBillingSlotProgress() {
+  const now = new Date();
+  const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+  const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+  const totalDaysInMonth = endOfMonth.getDate() - startOfMonth.getDate() + 1;
+  const millisSinceStartOfMonth = now.getTime() - startOfMonth.getTime();
+  return millisSinceStartOfMonth / (totalDaysInMonth * 1000 * 60 * 60 * 24);
+}
+
 async function getResponse(channel_id, user_id, message, model = "gpt-4") {
   // https://platform.openai.com/docs/guides/chat/introduction
   if (!process.env.OPENAI_API_KEY) return null;
+  if (!await canGetResponse(1.0)) return null;
+  if (!await canGetResponse(0.9)) model = "gpt-3.5-turbo";
+
   const conversation_key = (channel_id && user_id) ? `conversation:channel:${channel_id}:user:${user_id}` : null;
   let conversation = conversation_key ? await memory.get(conversation_key, []) : [];
-  conversation.push({ role: 'user', content: message });
+
+  let input = { role: 'user', content: message };
+  conversation.push(input);
   let response = await curl.request({
-    hostname: 'api.openai.com',
-    path: '/v1/chat/completions',
-    headers: { 'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY },
-    method: 'POST',
-    body: {
-      "model": model, // "gpt-3.5-turbo"
-      "messages": [{ "role": "system", "content": "Your name is Philbot. You are a Discord bot. You are depressed." }].concat(conversation.slice(-6))
-    },
-    timeout: 1000 * 60 * 3
-  }).then(result => result.choices[0].message);
-  conversation.push(response);
+      hostname: 'api.openai.com',
+      path: '/v1/chat/completions',
+      headers: { 'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY },
+      method: 'POST',
+      body: {
+        "model": model,
+        "messages": [{ "role": "system", "content": "My name is Philbot. I am a Discord bot. I am depressed." }].concat(conversation.slice(-6))
+      },
+      timeout: 1000 * 60 * 3
+    });
+  let output = response.choices[0].message;
+
+  conversation.push(output);
   if (conversation_key) await memory.set(conversation_key, conversation, 60 * 60 * 24 * 7);
-  return response.content;
+
+  let cost = await getCurrentCost()
+  cost.value += computeCost(model, response.usage.prompt_tokens, response.usage.completion_tokens);
+  cost.timestamp = Date.now();
+  await memory.set(costkey(), cost);
+
+  return output.content;
 }
 
-module.exports = { getResponse }
+async function getCurrentCost() {
+  let backup = { value: 0, timestamp: Date.now() };
+  let cost = await memory.get(costkey(), backup);
+  return new Date(cost.timestamp).getUTCMonth() != new Date().getUTCMonth() ? backup : cost;
+}
+
+function costkey() {
+  return 'openai:cost';
+}
+
+function computeCost(model, tokens_prompt, tokens_completion) {
+  switch (model) {
+    case "gpt-3.5-turbo":
+      return (tokens_prompt + tokens_completion) / 1000 * 0.002;
+    case "gpt-4":
+      return tokens_prompt / 1000 * 0.03 + tokens_completion / 1000 * 0.06;
+    default:
+      throw new Error();
+  }
+}
+
+module.exports = { getResponse, canGetResponse }
