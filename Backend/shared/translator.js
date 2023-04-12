69c15cc94c9520e89175f335a12df5d34cc16114
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2023-04-12 13:12:35 +0200

    improved translator

--- Backend/shared/translator.js
+++ Backend/shared/translator.js
@@ -11,7 +11,9 @@ async function on_message_create(guild_id, channel_id, message_id, content) {
   let target_language = await memory.get(memorykey(guild_id, channel_id), null);
   if (!target_language) return;
   if (!chatgpt.canGetResponse()) return;
-  let source_language = await chatgpt.getResponse(null, null, `What language is "${content}"? Respond with the language only.`);
+  let is_target_language = await chatgpt.getResponse(null, null, `Is "${content}" ${target_language}? Respond only with yes or no.`, "gpt-3.5-turbo");
+  if (!is_target_language || is_target_language.toLowerCase().trim().includes('yes')) return;
+  let source_language = await chatgpt.getResponse(null, null, `What language is "${content}"? Prefer ${target_language} if its ${target_language}. Respond only with the language. Ignore typos.`);
   if (!source_language || source_language.toLowerCase().trim() == target_language.toLowerCase().trim()) return;
   let translation = await chatgpt.getResponse(null, null, `Translate "${content}" to ${target_language}. Do not translate emojis, or parts that are surrounded by : < or >. Respond with the translation only, or nothing at all if the text is already in ${target_language} or untranslatable.`);
   if (!translation || translation.length == 0 || translation.trim() == content.trim()) return;
