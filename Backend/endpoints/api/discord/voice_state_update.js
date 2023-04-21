8e588063f12bfe805e0413a72600a0c02d8166e4
philipp.lengauer <p.lengauer@gmail.com>
2023-04-21 08:27:05 +0200

    shipping of server members

--- Backend/endpoints/api/discord/voice_state_update.js
+++ Backend/endpoints/api/discord/voice_state_update.js
@@ -13,7 +13,8 @@ async function handle(payload) {
     payload.channel_id ? checkAndStartEvents(payload.guild_id, payload.channel_id) : Promise.resolve(),
     discord.me().then(me => me.id == payload.user_id ? player.on_voice_state_update(payload.guild_id, payload.channel_id, payload.session_id) : Promise.resolve()),
     features.isActive(payload.guild_id, 'role management').then(active => active ? role_management.on_voice_state_update(payload.guild_id, payload.user_id, payload.channel_id) : Promise.resolve()),
-    payload.channel_id ? guessActivities(payload.guild_id, payload.channel_id, payload.user_id) : Promise.resolve()
+    payload.channel_id ? guessActivities(payload.guild_id, payload.channel_id, payload.user_id) : Promise.resolve(),
+    features.isActive(guild_id, 'player').then(active => active ? checkAndPlayShip(payload.guild_id, payload.channel_id, payload.user_id) : Promise.resolve()),
   ]).then(() => undefined);
 }
 
@@ -71,6 +72,30 @@ async function guessActivities(guild_id, channel_id, user_id) {
         memory.set(`activities:all:user:${user_id}`, global_activities.concat(guessed.filter(activity => !global_activities.includes(activity))), 60 * 60 * 24 * 31) :
         Promise.resolve()
     )
+  ]);
+}
+
+async function checkAndPlayShip(guild_id, channel_id, user_id) {
+  if (!channel_id) return;
+  let user_ids_in_voice_channel = await discord.guild_members_list(guild_id)
+    .then(members => members.map(member => member.user.id))
+    .then(other_user_ids => other_user_ids.map(other_user_id =>
+        memory.get(`voice_channel:user:${other_user_id}`)
+          .then(voice_state => voice_state && voice_state.guild_id == guild_id && voice_state.channel_id == channel_id ? other_user_id : null)
+      )
+    ).then(promises => Promise.all(promises))
+    .then(other_user_ids => other_user_ids.filter(other_user_id => !!other_user_id).filter(other_user_id => other_user_id != user_id));
+  if (user_ids_in_voice_channel.length != 1) return;
+  let other_user_id = user_ids_in_voice_channel[0];
+  let shipped = await memory.get(`ship:user:${user_id}:user:${other_user_id}`, false) || await memory.get(`ship:user:${other_user_id}:user:${user_id}`, false);
+  if (!shipped) return;
+  const music = [ 'Marvin Gaye Lets get it on', 'Joe Cocker You can leave your hat on', 'George Michael Careless Whisper' /*, 'Foreigner I dont know what love is' */ ];
+  return Promise.all([
+    player.play(guild_id, channel_id, music[Math.floor(Math.random() * music.length)]),
+    discord.try_dms(user_id, 'Congratulations, you have been shipped with ' + discord.mention_user(other_user_id) + ' by one of your fellow server members!'),
+    discord.try_dms(other_user_id, 'Congratulations, you have been shipped with ' + discord.mention_user(user_id) + ' by one of your fellow server members!'),
+    memory.unset(`ship:user:${user_id}:user:${other_user_id}`),
+    memory.unset(`ship:user:${other_user_id}:user:${user_id}`),
   ]);
 }
 
