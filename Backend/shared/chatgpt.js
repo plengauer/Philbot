ecd4c6878e98c489a0909f143f94daa3d8c98198
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2023-04-25 12:10:18 +0200

    fix 32k model usage

--- Backend/shared/chatgpt.js
+++ Backend/shared/chatgpt.js
@@ -6,9 +6,10 @@ const opentelemetry = require('@opentelemetry/api');
 const cost_limit = parseFloat(process.env.OPENAI_API_COST_LIMIT ?? '1.00');
 
 const meter = opentelemetry.metrics.getMeter('openai');
-const cost_absolute_counter = meter.createHistogram('openai.cost.absolute');
-const cost_relative_counter = meter.createHistogram('openai.cost.relative');
-const cost_progress_counter = meter.createHistogram('openai.cost.progress');
+const cost_counter = meter.createCounter('openai.cost');
+const cost_absolute_counter = meter.createHistogram('openai.cost.slotted.absolute');
+const cost_relative_counter = meter.createHistogram('openai.cost.slotted.relative');
+const cost_progress_counter = meter.createHistogram('openai.cost.slotted.progress');
 
 async function canGetResponse(threshold = 0.8) {
   return (await getCurrentCost()).value / cost_limit * threshold < computeBillingSlotProgress();
@@ -53,13 +54,15 @@ async function getResponse(channel_id, user_id, message, model = "gpt-4") {
   if (conversation_key) await memory.set(conversation_key, conversation, 60 * 60 * 24 * 7);
 
   let cost = await getCurrentCost()
-  cost.value += computeCost(model, response.usage.prompt_tokens, response.usage.completion_tokens);
+  cost.value += computeCost(response.model.replace(/-\d\d\d\d$/, ''), response.usage.prompt_tokens, response.usage.completion_tokens);
   cost.timestamp = Date.now();
   await memory.set(costkey(), cost);
 
-  cost_absolute_counter.record(cost.value, {});
-  cost_relative_counter.record(cost.value / cost_limit, {});
-  cost_progress_counter.record(cost.value / (cost_limit * computeBillingSlotProgress()), {});
+  const attributes = { model: response.model };
+  cost_counter.add(cost, attributes);
+  cost_absolute_counter.record(cost.value, attributes);
+  cost_relative_counter.record(cost.value / cost_limit, attributes);
+  cost_progress_counter.record(cost.value / (cost_limit * computeBillingSlotProgress()), attributes);
 
   return output.content;
 }
@@ -80,6 +83,8 @@ function computeCost(model, tokens_prompt, tokens_completion) {
       return (tokens_prompt + tokens_completion) / 1000 * 0.002;
     case "gpt-4":
       return tokens_prompt / 1000 * 0.03 + tokens_completion / 1000 * 0.06;
+    case "gpt-4-32k":
+      return tokens_prompt / 1000 * 0.06 + tokens_completion / 1000 * 0.12;
     default:
       throw new Error();
   }
