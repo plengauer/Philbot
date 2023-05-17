e8677fb8f3e62c47f529d3d7ef6bee1d8ae128bc
philipp.lengauer <p.lengauer@gmail.com>
2023-05-17 22:32:31 +0200

    handle error messages

--- Backend/endpoints/api/discord/message_create.js
+++ Backend/endpoints/api/discord/message_create.js
@@ -904,7 +904,7 @@ async function handleCommand(guild_id, channel_id, event_id, user_id, user_name,
     return handleLongResponse(channel_id, () => chatgpt.createImage(message)
       .then(image => image ? image : Promise.reject('I couldn\'t draw the picture!'))
       .then(file => discord.post(channel_id, '', event_id, true, [{ image: { url: 'attachment://image.png' } }], [], [{ filename: 'image.png', description: message, content: file }]))
-      .catch(error => discord.post(channel_id, event_id, error.message))
+      .catch(error => discord.respond(channel_id, event_id, error.message))
     );
 
   } else if (message == 'mirror' || message.startsWith('mirror to ')) {
