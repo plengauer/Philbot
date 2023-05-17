a744e1dbb5bdacad8cdd3499143c8fdb09d75b78
philipp.lengauer <p.lengauer@gmail.com>
2023-05-17 22:11:04 +0200

    fix createBoolean

--- Backend/shared/openai.js
+++ Backend/shared/openai.js
@@ -147,10 +147,10 @@ async function createBoolean(question, model) {
   if (!token) return null;
   if (!await canCreate()) return null;
   let response = await createCompletion(`Answer the question only with yes or no.\nQuestion: ${question}\nAnswer:`, model);
-  response = response.toLowerCase();
+  response = response.trim().toLowerCase();
   const match = response.match(/^([a-z]+)/);
   response = match ? match[0] : response;
-  if (response != 'yes' && response != 'no') throw new Error('Response is not a bool!');
+  if (response != 'yes' && response != 'no') throw new Error('Response is not a bool! (' + response + ')');
   return response == 'yes'; 
 }
 
