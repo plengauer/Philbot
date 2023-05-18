f81489c6a35c00141d7cc5909a9791a7504e8400
plengauer <100447901+plengauer@users.noreply.github.com>
2023-05-18 22:24:34 +0200

    Dropped async on image sizes

--- Backend/shared/openai.js
+++ Backend/shared/openai.js
@@ -164,7 +164,7 @@ async function createBoolean(question, model = undefined) {
 
 const IMAGE_SIZES = ["256x256", "512x512", "1024x1024"];
 
-async function getImageSizes() {
+function getImageSizes() {
   return IMAGE_SIZES;
 }
 
