2694b5ab42b9ec6b5ef38e0c3a765d155a9e270f
plengauer <100447901+plengauer@users.noreply.github.com>
2023-05-19 09:29:07 +0200

    mirror

--- Backend/endpoints/api/discord/message_reaction_add.js
+++ Backend/endpoints/api/discord/message_reaction_add.js
@@ -1,11 +1,13 @@
 const discord = require('../../../shared/discord.js');
 const features = require('../../../shared/features.js');
 const role_management = require('../../../shared/role_management.js');
+const mirror = require('../../../shared/mirror.js');
 
 async function handle(payload) {
-  return features.isActive(payload.guild_id, "role management")
-    .then(active => active ? role_management.on_reaction_add(payload.guild_id, payload.user_id) : Promise.resolve())
-    .then(() => undefined);
+  return Promise.all([
+    mirror.on_reaction_add(payload.guild_id, payload.channel_id, payload.user_id, payload.message_id, payload.emoji),
+    features.isActive(payload.guild_id, "role management").then(active => active ? role_management.on_reaction_add(payload.guild_id, payload.user_id) : Promise.resolve())
+  ]).then(() => undefined);
 }
 
 module.exports = { handle }
