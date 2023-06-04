820f2df6ba7dfe63ec8b4451aa4442bd598e33fb
philipp.lengauer <p.lengauer@gmail.com>
2023-06-04 13:14:58 +0200

    apply auto roles for all if we are restarting

--- Backend/endpoints/api/discord/guild_create.js
+++ Backend/endpoints/api/discord/guild_create.js
@@ -1,17 +1,17 @@
 const discord = require('../../../shared/discord.js');
+const role_management = require('../../../shared/role_management.js');
 
 async function handle(payload) {
-  if (new Date(payload.joined_at).getTime() < Date.now() - 1000 * 10) {
-    return undefined;
-  }
   let guild_id = payload.id;
-  return Promise.all([
-      discord.guild_retrieve(guild_id),
-      discord.me()
-    ]).then(values => values[0].system_channel_id ?
-      discord.post(values[0].system_channel_id, `Hi, I'm <@${values[1].id}>. I can play music, tell jokes, schedule weekly events, whatever you need. Type \'<@${values[1].id}> help\' to learn how to talk to me. I'm always around and happy to help.`) :
-      Promise.resolve()
-    ).then(() => undefined);
+  if (new Date(payload.joined_at).getTime() >= Date.now() - 1000 * 10) {
+    let guild = await discord.guild_retrieve(guild_id);
+    if (guild.system_channel_id) {
+      let me = await discord.me();
+      await discord.post(values[0].system_channel_id, `Hi, I'm <@${me.id}>. I can play music, tell jokes, schedule weekly events, whatever you need. Type \'<@${me.id}> help\' to learn how to talk to me. I'm always around and happy to help.`);
+    }
+  } else {
+    await role_management.update_all(guild_id);
+  }
 }
 
 module.exports = { handle }
