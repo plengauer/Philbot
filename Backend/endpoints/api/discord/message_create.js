a48277b8bd5d709b3b84fda87f7e00db44b5dac4
philipp.lengauer <p.lengauer@gmail.com>
2023-06-11 13:33:48 +0200

    join right channel

--- Backend/endpoints/api/discord/message_create.js
+++ Backend/endpoints/api/discord/message_create.js
@@ -330,7 +330,9 @@ async function handleCommand(guild_id, channel_id, event_id, user_id, message, r
   } else if (message == 'join') {
     guild_id = guild_id ?? await resolveGuildID(user_id);
     if (!guild_id) return reactNotOK(channel_id, event_id);
-    let voice_channel_id = await memory.get(`player:voice_channel:guild:${guild_id}`, undefined);
+    let voice_state = await memory.get(`voice_channel:user:${payload.user_id}`, null);
+    if (!voice_state || voice_state.guild_id != guild_id) return reactNotOK(channel_id, event_id);
+    let voice_channel_id = voice.state.channel_id;
     if (!voice_channel_id) return reactNotOK(channel_id, event_id);
     return discord.connect(guild_id, voice_channel_id)
       .then(() => reactOK(channel_id, event_id));
