3c0427877ac6b946f8407be1320da761813b1ee5
philipp.lengauer <p.lengauer@gmail.com>
2023-06-18 14:48:56 +0200

    better merging

--- Backend/shared/audio.js
+++ Backend/shared/audio.js
@@ -3,16 +3,24 @@ const child_process = require('child_process');
 const { PassThrough } = require('stream');
 
 const DEBUG = (process.env.DEBUG_AUDIO ?? 'false') == 'true';
+const MERGE_STRATEGY = parseInt(process.env.AUDIO_MERGE_STRATEGY ?? '1');
 
 function translate(input_stream, input_format, output_format) {
   if (input_format == output_format) return input_stream;
-  let convertion = child_process.spawn("ffmpeg", ["-i", "pipe:0", "-f", output_format, "pipe:1"]);
+  let convertion = ffmpeg(['-i', 'pipe:0', '-f', output_format, 'pipe:1']);
   input_stream.pipe(convertion.stdin);
-  if (DEBUG) convertion.stderr.on('data', chunk => console.log('' + chunk));
   return convertion.stdout;
 }
 
 function merge(inputs, output_format) {
+  switch(MERGE_STRATEGY) {
+    case 0: return merge_v0(inputs, output_format);
+    case 1: return merge_v1(inputs, output_format);
+    default: throw new Error('Unknown merging strategy: ' + MERGE_STRATEGY);
+  }
+}
+
+function merge_v0(inputs, output_format) {
   if (inputs.length == 0) throw new Error();
 
   for (let input of inputs) {
@@ -31,6 +39,34 @@ function merge(inputs, output_format) {
   inputs[0].stream.pipe(merged, { end: false });
 
   return translate(merged, 'wav', output_format);
+}
+
+function merge_v1(inputs, output_format) {
+  if (inputs.length == 0) throw new Error();
+  // ffmpeg -i input1.wav -i input2.wav -i input3.wav -i input4.wav -filter_complex '[0:0][1:0][2:0][3:0]concat=n=4:v=0:a=1[out]' -map '[out]' output.wav
+  // ffmpeg -i pipe:3 -i pipe:4 -filter_complex '[0:0][1:0]concat=n=2:v=0:a=1[out]' -f mp3 -map '[out]' pipe:1
+  let input_arguments = [];
+  let channel_arguments = [];
+  const first_pipe = 3;
+  for (let index = 0; index < inputs.length; index++) {
+    input_arguments.push('-i');
+    input_arguments.push('pipe:' + (first_pipe + index));
+    channel_arguments.push('[' + index + ':0]');
+  }
+  let merging = ffmpeg(
+    input_arguments.concat(['-filter_complex', channel_arguments.join('') + 'concat=n=' + inputs.length + ':v=0:a=1[out]', '-f', output_format, '-map', '[out]', 'pipe:1'])
+    ['pipe', 'pipe', 'pipe'].concat(inputs.map(_ => 'pipe'))
+  );
+  for (let index = 0; index < inputs.length; index++) {
+    inputs[index].stream.pipe(merging.stdio[first_pipe + index]);
+  }
+  return merging.stdout;
+}
+
+function ffmpeg(arguments, stdio = ['pipe', 'pipe', 'pipe']) {
+  let process = child_process.spawn('ffmpeg', arguments, { stdio: stdio });
+  if (DEBUG) process.stderr.on('data', chunk => console.log('' + chunk));
+  return process;
 }
 
 module.exports = { translate, merge }
