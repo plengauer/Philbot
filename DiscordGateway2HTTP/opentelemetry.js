e0455555efcb2fcb0f6c9c6ec5eece73224830a1
philipp.lengauer <p.lengauer@gmail.com>
2023-07-07 11:16:58 +0200

    otel changed its way to import things

--- DiscordGateway2HTTP/opentelemetry.js
+++ DiscordGateway2HTTP/opentelemetry.js
@@ -1,14 +1,13 @@
 import propertiesReader from 'properties-reader';
 import opentelemetry_api from '@opentelemetry/api';
 import opentelemetry_sdk from "@opentelemetry/sdk-node";
+import opentelemetry_tracing from "@opentelemetry/sdk-trace-base";
+import opentelemetry_resources from "@opentelemetry/resources";
+import opentelemetry_semantic_conventions from "@opentelemetry/semantic-conventions";
 
-import { Resource } from '@opentelemetry/resources';
-import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { PeriodicExportingMetricReader, AggregationTemporality} from '@opentelemetry/sdk-metrics';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
-import { AlwaysOnSampler, AlwaysOffSampler } from "@opentelemetry/core";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 
 class ShutdownAwareSpanProcessor {
@@ -52,16 +51,16 @@ async function init() {
 function create() {
   let name = process.env.SERVICE_NAME;
   let version = process.env.SERVICE_VERSION;
-  let dtmetadata = new Resource({});
+  let dtmetadata = new opentelemetry_resources.Resource({});
   for (let name of ['dt_metadata_e617c525669e072eebe3d0f08212e8f2.properties', '/var/lib/dynatrace/enrichment/dt_metadata.properties']) {
     try {
-      dtmetadata.merge(new Resource(propertiesReader(name.startsWith("/var") ? name : fs.readFileSync(name).toString()).getAllProperties()));
+      dtmetadata.merge(new opentelemetry_resources.Resource(propertiesReader(name.startsWith("/var") ? name : fs.readFileSync(name).toString()).getAllProperties()));
     } catch { }
   }
   const sdk = new opentelemetry_sdk.NodeSDK({
-    sampler: (name && version) ? new AlwaysOnSampler() : new AlwaysOffSampler(),
+    sampler: (name && version) ? new opentelemetry_tracing.AlwaysOnSampler() : new opentelemetry_tracing.AlwaysOffSampler(),
     spanProcessor: new ShutdownAwareSpanProcessor(
-      new BatchSpanProcessor(
+      new opentelemetry_tracing.BatchSpanProcessor(
         new OTLPTraceExporter({
           url: process.env.OPENTELEMETRY_TRACES_API_ENDPOINT,
           headers: { Authorization: "Api-Token " + process.env.OPENTELEMETRY_TRACES_API_TOKEN },
@@ -77,9 +76,9 @@ function create() {
       exportIntervalMillis: 5000,
     }),
     instrumentations: [getNodeAutoInstrumentations({'@opentelemetry/instrumentation-fs': { enabled: false }})],
-    resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: name,
-        [SemanticResourceAttributes.SERVICE_VERSION]: version,
+    resource: new opentelemetry_resources.Resource({
+        [opentelemetry_semantic_conventions.SemanticResourceAttributes.SERVICE_NAME]: name,
+        [opentelemetry_semantic_conventions.SemanticResourceAttributes.SERVICE_VERSION]: version,
       }).merge(dtmetadata),
   });
   return sdk;
