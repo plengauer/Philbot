eb0a8b9226eb0dbfafbd791fb8474932f403cd05
philipp.lengauer <p.lengauer@gmail.com>
2023-07-11 21:59:09 +0200

    fix autoplay on playback finished

--- Backend/endpoints/api/discord/voice_playback_finished.js
+++ Backend/endpoints/api/discord/voice_playback_finished.js
@@ -1,7 +1,7 @@
 const player = require('../../../shared/player.js');
 
 async function handle(payload) {
-  return player.playNext(payload.guild_id, true).then(() => undefined);
+  return player.playNext(payload.guild_id, undefined, true).then(() => undefined);
 }
 
 module.exports = { handle }
