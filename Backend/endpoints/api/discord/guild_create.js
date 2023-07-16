419b493c81a641d4cfbb921aab376645f7fbae0e
philipp.lengauer <p.lengauer@gmail.com>
2023-07-16 21:11:24 +0200

    fixed guild create

--- Backend/endpoints/api/discord/guild_create.js
+++ Backend/endpoints/api/discord/guild_create.js
@@ -8,7 +8,7 @@ async function handle(payload) {
     let guild = await discord.guild_retrieve(guild_id);
     if (guild.system_channel_id) {
       let me = await discord.me();
-      await discord.post(values[0].system_channel_id, `Hi, I'm <@${me.id}>. I can play music, tell jokes, schedule weekly events, whatever you need. Type \'<@${me.id}> help\' to learn how to talk to me. I'm always around and happy to help.`);
+      await discord.post(guild.system_channel_id, `Hi, I'm <@${me.id}>. I can play music, tell jokes, schedule weekly events, whatever you need. Type \'<@${me.id}> help\' to learn how to talk to me. I'm always around and happy to help.`);
     }
   } else {
     if (await features.isActive(guild_id, "role management")) {
