959079637c5db42f26c18d6c99cbf028042af9e3
philipp.lengauer <p.lengauer@gmail.com>
2023-08-16 22:12:52 +0200

    added missing config file

--- Setup/containerized_run_scheduler.sh
+++ Setup/containerized_run_scheduler.sh
@@ -1,3 +1,8 @@
+BACKEND_PORT=8080
+echo 'minutely=http://127.0.0.1:'$BACKEND_PORT'/scheduler/minutely' >> ./config.properties.scheduler &&
+echo 'hourly=http://127.0.0.1:'$BACKEND_PORT'/scheduler/hourly' >> ./config.properties.scheduler &&
+echo 'daily=http://127.0.0.1:'$BACKEND_PORT'/scheduler/daily' >> ./config.properties.scheduler &&
+echo 'monthly=http://127.0.0.1:'$BACKEND_PORT'/scheduler/monthly' >> ./config.properties.scheduler &&
 sudo docker run \
     -env-file environment.properties.scheduler \
     --env CONFIG_FILE=/config.properties \
