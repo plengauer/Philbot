4d21ac379c5b51aea11885db230f254d5cbde4d1
philipp.lengauer <p.lengauer@gmail.com>
2023-08-16 23:00:32 +0200

    enable networking between containers

--- Setup/containerized_run_voice.sh
+++ Setup/containerized_run_voice.sh
@@ -2,6 +2,7 @@ CONTAINER_CACHE_DIRECTORY=audio_cache
 HOST_CACHE_DIRECTORY=.philbot_voice_$CONTAINER_CACHE_DIRECTORY
 mkdir -p $HOST_CACHE_DIRECTORY &&
 sudo docker run \
+    --network="host" \
     --env-file environment.properties.voice \
     --env CACHE_DIRECTORY=/$CONTAINER_CACHE_DIRECTORY \
     --mount type=bind,source=$(pwd)/$HOST_CACHE_DIRECTORY,target=/$CONTAINER_CACHE_DIRECTORY \
