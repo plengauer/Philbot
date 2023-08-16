a889aef41eff4ed88b692af1eb70636158ec8cd2
philipp.lengauer <p.lengauer@gmail.com>
2023-08-16 17:50:08 +0200

    removed exec

--- Setup/containerized_run_discordgateway2http.sh
+++ Setup/containerized_run_discordgateway2http.sh
@@ -1,7 +1,7 @@
 CONTAINER_SESSIONS_DIRECTORY=sessions
 HOST_SESSIONS_DIRECTORY=.philbot_discordgateway2http_$CONTAINER_SESSIONS_DIRECTORY
 mkdir -p $HOST_SESSIONS_DIRECTORY &&
-sudo exec docker run -d philipplengauer/philbot-discordgateway2http:latest \
+sudo docker run -d philipplengauer/philbot-discordgateway2http:latest \
     -env-file environment.properties.discordgateway2http \
     --mount type=bind,source=$HOST_SESSIONS_DIRECTORY,target=$CONTAINER_SESSIONS_DIRECTORY \
     --env STATE_STORAGE_DIRECTORY=$HOST_SESSIONS_DIRECTORY \
--- Setup/containerized_run_scheduler.sh
+++ Setup/containerized_run_scheduler.sh
@@ -1,4 +1,4 @@
-sudo exec docker run -d philipplengauer/philbot-scheduler:latest \
+sudo docker run -d philipplengauer/philbot-scheduler:latest \
     -env-file environment.properties.scheduler \
     --env CONFIG_FILE=/config.properties \
     --mount type=bind,source=config.properties.scheduler,target=/config.properties,readonly
