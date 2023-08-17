1183f4a896794c843d2d9048e474fa1ea7f35b13
philipp.lengauer <p.lengauer@gmail.com>
2023-08-17 13:24:14 +0200

    fixed path

--- Setup/containerized_run_discordgateway2http.sh
+++ Setup/containerized_run_discordgateway2http.sh
@@ -5,7 +5,7 @@ exec sudo docker run \
     --user $(id -u):$(id -g) \
     --network="host" \
     --env-file environment.properties.discordgateway2http \
-    --env STATE_STORAGE_DIRECTORY=/$HOST_SESSIONS_DIRECTORY \
+    --env STATE_STORAGE_DIRECTORY=/$CONTAINER_SESSIONS_DIRECTORY \
     --mount type=bind,source=$(pwd)/$HOST_SESSIONS_DIRECTORY,target=/$CONTAINER_SESSIONS_DIRECTORY \
     --env SHARD_INDEX=$SHARD_INDEX \
     --env SHARD_COUNT=$SHARD_COUNT \
