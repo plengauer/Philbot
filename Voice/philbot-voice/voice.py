9c93efae2c73c51ba2d74e581a57b37af471a5ec
philipp.lengauer <p.lengauer@gmail.com>
2023-09-02 15:27:27 +0200

    use token

--- Voice/philbot-voice/voice.py
+++ Voice/philbot-voice/voice.py
@@ -49,11 +49,9 @@ class AwsEC2ResourceDetector(ResourceDetector):
     def detect(self) -> Resource:
         context_token = attach(set_value(_SUPPRESS_INSTRUMENTATION_KEY, True))
         try:
-            # token = requests.put('http://169.254.169.254/latest/api/token', headers={ 'X-aws-ec2-metadata-token-ttl-seconds': '60' }, timeout=5).text
-            # identity = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document', headers={ 'X-aws-ec2-metadata-token': token }, timeout=5).json()
-            # hostname = requests.get('http://169.254.169.254/latest/meta-data/hostname', headers={ 'X-aws-ec2-metadata-token': token }, timeout=5).text
-            identity = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document', timeout=5).json()
-            hostname = requests.get('http://169.254.169.254/latest/meta-data/hostname', timeout=5).text
+            token = requests.put('http://169.254.169.254/latest/api/token', headers={ 'X-aws-ec2-metadata-token-ttl-seconds': '60' }, timeout=5).text
+            identity = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document', headers={ 'X-aws-ec2-metadata-token': token }, timeout=5).json()
+            hostname = requests.get('http://169.254.169.254/latest/meta-data/hostname', headers={ 'X-aws-ec2-metadata-token': token }, timeout=5).text
             return Resource.create({
                 'cloud.provider': 'aws',
                 'cloud.platform': 'aws_ec2',
