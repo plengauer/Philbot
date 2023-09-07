b408c3d9a7db3ac5f86b5389b0d69675e8ee9b53
philipp.lengauer <p.lengauer@gmail.com>
2023-09-07 23:05:05 +0200

    prompt for configs

--- Deployment/Baremetal/usr/local/sbin/genericstart.sh
+++ Deployment/Baremetal/usr/local/sbin/genericstart.sh
@@ -1,36 +1,37 @@
+#!/bin/bash
+set -e
 directory=$1
 env_file=$2
 technology=$3
 module=$4
 lock_file=$(pwd)/.install_lock
-touch $lock_file &&
-cd $directory &&
-eval $(cat $env_file | awk '{print "export \"" $0 "\""}') ||
-exit $?
+touch $lock_file
+cd $directory
+eval $(cat $env_file | awk '{print "export \"" $0 "\""}')
 if [ $technology = "node.js" ]
 then
     package=$module
-    ([ -e node_modules/ ] || flock -F $lock_file npm install $package) &&
-    flock -F $lock_file npm update &&
-    export SERVICE_VERSION=$(cat node_modules/$package/package.json | jq -r .version) &&
+    ([ -e node_modules/ ] || flock -F $lock_file npm install $package)
+    flock -F $lock_file npm update
+    export SERVICE_VERSION=$(cat node_modules/$package/package.json | jq -r .version)
     exec npm --prefix node_modules/$package run-script exec-start
 elif [ $technology = "ruby" ]
 then
     gem=$module
-    rm -rf gems &&
-    mkdir -p gems &&
-    flock -F $lock_file gem install --install-dir=gems $gem &&
-    pushd gems/gems/$gem-*/ && flock -F $lock_file bundle install && popd &&
-    export SERVICE_VERSION=$(ls gems/gems/$gem-* | awk -F- '{print $2}') &&
+    rm -rf gems
+    mkdir -p gems
+    flock -F $lock_file gem install --install-dir=gems $gem
+    pushd gems/gems/$gem-*/ && flock -F $lock_file bundle install && popd
+    export SERVICE_VERSION=$(ls gems/gems/$gem-* | awk -F- '{print $2}')
     exec ruby gems/gems/$gem-*/lib/*.rb
 elif [ $technology = "python" ]
 then
     package=$module
     python3 -m venv venv
-    source venv/bin/activate &&
-    flock -F $lock_file pip3 install $package --upgrade &&
-    export PYTHONPATH=$(find venv/lib/python*/site-packages/$package -prune | tr '\n' ':') &&
-    export SERVICE_VERSION=$(pip show $package | grep Version | cut -d':' -f2 | sed 's/ //g') &&
+    source venv/bin/activate
+    flock -F $lock_file pip3 install $package --upgrade
+    export PYTHONPATH=$(find venv/lib/python*/site-packages/$package -prune | tr '\n' ':')
+    export SERVICE_VERSION=$(pip show $package | grep Version | cut -d':' -f2 | sed 's/ //g')
     exec venv/bin/opentelemetry-instrument python3 -u -m $package
 elif [ $technology = "bash" ]
 then
