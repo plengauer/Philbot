d69413040750701c7153a248cbb3a1d4717ec252
philipp.lengauer <p.lengauer@gmail.com>
2023-09-09 16:32:57 +0200

    fixed shard count

--- Deployment/Containerized/etc/philbot-containerized/shard_count.sh
+++ Deployment/Containerized/etc/philbot-containerized/shard_count.sh
@@ -3,4 +3,4 @@ set -e
 prefix=$1
 . /usr/share/debconf/confmodule
 db_get philbot-containerized/"$@"
-echo $(echo $prefix_$(seq -s " $prefix_" 1 $(($RET-1))) "$prefix"_0 | tr ' ' '\n' | sort -n -t _ -k 2 | tr '\n' ' ')
+echo "$RET"
