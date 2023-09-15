fb38b91a617e1a675ce25934ed51aa8423a1d42a
philipp.lengauer <p.lengauer@gmail.com>
2023-09-15 14:38:03 +0200

    fixed length check

--- Backend/endpoints/api/discord/message_create.js
+++ Backend/endpoints/api/discord/message_create.js
@@ -34,7 +34,7 @@ async function handle0(guild_id, channel_id, event_id, user_id, message, referen
 
   if (is_voice_message && await memory.consume(`voice_clone:in_progress:user:${user_id}`, false)) {
     let attachment = attachments[0];
-    if (attachments.duration_secs < 60) return respond(guild_id, channel_id, event_id, 'The voice sample is too short!');
+    if (attachment.duration_secs < 60) return respond(guild_id, channel_id, event_id, 'The voice sample is too short!');
     let format = attachment.content_type.split('/')[1];
     await memory.set(`voice_clone:sample:user:${user_id}`, attachment);
     await ai.seedVoice(null, user_id, await streamAttachment(attachment), format);
