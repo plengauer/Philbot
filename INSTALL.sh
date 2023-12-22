e998ec6d174ecebfc028f7a87ed426e6bb98c6fb
philipp.lengauer <p.lengauer@gmail.com>
2023-12-22 12:51:17 +0100

    re-introduce recommended package

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,4 +2,4 @@
 set -e
 echo "deb [arch=all] http://philbot.eu:8000/ stable main" | sudo tee /etc/apt/sources.list.d/example.list
 sudo apt-get update --allow-insecure-repositories
-sudo apt-get install -y --allow-unauthenticated philbot
+sudo apt-get install -y --allow-unauthenticated philbot-recommended
