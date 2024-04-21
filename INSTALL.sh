ec8c7ea57c148c1c5c6428ef5e55b2c912720ffc
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-21 11:56:50 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,4 +2,4 @@
 set -e
 echo "deb [arch=all] http://philbot.eu:8000/ stable main" | sudo tee /etc/apt/sources.list.d/philbot.list
 sudo apt-get update --allow-insecure-repositories
-sudo apt-get install -y --allow-unauthenticated philbot
+sudo -E apt-get install -y --allow-unauthenticated philbot
