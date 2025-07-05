88efaedb253566b8d546da13810cc81061e1a9f1
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-07-05 15:25:47 +0200

    Update games.js

--- Backend/shared/games/games.js
+++ Backend/shared/games/games.js
@@ -20,12 +20,6 @@ async function getActivityEmergencyNotification(name, details, state, user_name)
 async function getActivityHint(name, details, state, user_id) {
   if (name === 'Red Dead Redemption 2' || name.toLowerCase() === 'rdr2' || name.toLowerCase() === 'rdro') {
     return rdro.getInformation(3);
-  } else if (name === 'League of Legends' || name == 'Teamfight Tactics: League of Legends Strategy Game' || name.toLowerCase() === 'lol') {
-    return lol.getInformation(details, state, user_id);
-  } else if ((name.startsWith('World of Warcraft') && !name.startsWith('World of Warcraft Classic')) || name.toLowerCase().startsWith('wow')) {
-    // that this is part of the name is not kewl!
-    config = (name.startsWith('wow') ? name.split(' ').slice(1).join(' ') : name.split(' ').slice(3).join(' ')).trim();
-    return wow.getInformation(config);
   } else if (name === 'Epic Games' || name === 'Epic Games Launcher' || name === 'Epic Games Store') {
     return epicgames.getInformation();
   } else if (name.startsWith('Phasmophobia')) {
@@ -38,24 +32,13 @@ async function getActivityHint(name, details, state, user_id) {
     return fortnite.getInformation();
   } else if (name == 'Kerbal Space Program' || name == 'Kerbal Space Program 2') {
     return ksp.getInformation();
-  } else if (
-    name === 'Genshin Impact' ||
-    name === 'VALORANT' ||
-    name === "PLAYERUNKNOWN'S BATTLEGROUNDS" ||
-    name === "PUBG: BATTLEGROUNDS" ||
-    name === 'Overwatch' ||
-    name === 'Overwatch 2'
-  ) {
-    return troll.getInformation();
   } else {
     return null;
   }
 }
 
 async function updateRankedRoles(name, guild_id, user_id) {
-  if (name == 'League of Legends' || name == 'Teamfight Tactics: League of Legends Strategy Game') {
-    return lol.updateRankedRoles(guild_id, user_id);
-  } else if (name == 'Apex Legends') {
+  if (name == 'Apex Legends') {
     return apex.updateRankedRoles(guild_id, user_id);
   } else if (name == 'Fortnite') {
     return fortnite.updateRankedRoles(guild_id, user_id);
